4e60a7728e0439abaf0430f28da9512ade858435
CJ Clark <cjclark@CJs-MacBook-Air.local>
2020-11-16 16:27:42 -0600

    Created buttons in html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class Plant {
+    constructor(name, sunpower, h20, love) {
+        this.name = name;
+        this.sunpower = sunpower;
+        this.h20 = h20;
+        this.love = love;
+    }
+
+    updateStats() {
+        currentContainer.innerHTML = `
+            
+        `
+    }
+}
