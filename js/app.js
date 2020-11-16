fc52c2e3292b3bd9ec39e1c1999fa652c499a602
CJ Clark <cjclark@CJs-MacBook-Air.local>
2020-11-16 16:44:40 -0600

    updates to my buttons in HTML and JS

--- js/app.js
+++ js/app.js
@@ -2,13 +2,21 @@ class Plant {
     constructor(name, sunpower, h20, love) {
         this.name = name;
         this.sunpower = sunpower;
-        this.h20 = h20;
+        this.h2o = h2o;
         this.love = love;
     }
 
     updateStats() {
         currentContainer.innerHTML = `
-            
+        <div class="sun btn">
+                Sun Power: <span>${this.sunpower}</span>
+            </div>
+            <div class="water">
+                H2O: <span>${this.h2o}</span>
+            </div>
+            <div class="lve btn">
+                Love: <span>${this.love}</span>
+            </div>    
         `
     }
 }
